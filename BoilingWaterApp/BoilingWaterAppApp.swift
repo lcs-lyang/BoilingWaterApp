1cfc4b35b4ec9af7c9ee7fcb970b17e38c52ab67
Lillian Yang <lyang@lcs.on.ca>
2022-02-15 11:16:12 -0500

    Initial Commit

---
+++ BoilingWaterApp/BoilingWaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterAppApp.swift
+//  BoilingWaterApp
+//
+//  Created by Lillian Yang on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoilingWaterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoilingWaterApp
+//
+//  Created by Lillian Yang on 2022-02-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
